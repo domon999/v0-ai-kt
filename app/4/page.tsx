2408016208a1b95623a9e18f1a52cf899cee1415
domon999 <wxm544846051@gmail.com>
2026-02-27 23:40:54 +0800

    feat: add test page at /4

---
+++ app/4/page.tsx
@@ -0,0 +1,7 @@
+export default function Page4() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold">测试页面4</h1>
+    </div>
+  )
+}
